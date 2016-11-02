0f4432e930aa2b0bbc89186e9854a37bfd6c980e
Matthew Lee <matthew.lee711@gmail.com>
2016-11-01 23:36:23 -0500

    Finished hash picture generator

---
+++ phoenixNotes.exs
@@ -0,0 +1,9 @@
+# What is phoenix used for?
+# Acts as a webserver (digital ocean, etc)
+
+
+# HTML, JSON, Websockets (user receives)
+# phoenix
+# database (traditionally postgres)
+
+# How phoenix handles incoming requests
