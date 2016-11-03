f9b78da6d993558942da3527a060761b1148c118
Matthew Lee <matthew.lee711@gmail.com>
2016-11-03 15:25:40 -0500

    Able to add topics to db and display them on index

---
+++ dbstuff.exs
@@ -0,0 +1,49 @@
+Phoenix           Ecto
+Model             Changeset
+Controller        Repo
+
+
+Phoenix depends on ecto
+Ecto - deals with changeset stuff and something called a rapo
+Repo - portal to DB
+
+
+Controller <-> changeset <-> Repo <-> Postgres
+# In the Controller, we make a changeset which contains
+# the data the user is trying to insert
+# Changeset will then be inserted into DB by Repo
+
+Repo will autimatically check if changeset is valid
+
+# Look for errors under title
+<%= error_tag f, :title %>
+
+# In phoenix we're doing a POST request, not a client Side
+# ajax request
+
+
+#
+get "/", PageController, :index
+
+# ECTO!
+hexdocs.pm/ecto
+
+Querying in elixir ecto
+# Select everything in topics table
+Discuss.Repo.all(Discuss.Topic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,13 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  # Show all topics in html page
+  def index(conn, _params) do
+    topics = Discuss.Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  # Serve new.html page
   def new(conn, _params) do
     struct = %Topic{}
     params = %{}
@@ -10,7 +17,15 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
 
+  # Insert topic into DB -- handler
   def create(conn, params) do
     %{"topic" => topic} = params
+    changeset = Topic.changeset(%Topic{}, topic)
+    # Insert into DB success and failure
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
